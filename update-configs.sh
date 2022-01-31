9e85d0c95ed6ce3e208819a620fd2b6c95b5bab6
Andres Sewell <sewellandres@gmail.com>
2022-01-31 00:38:09 -0700

    Gtk-3 always prefer dark theme

--- update-configs.sh
+++ update-configs.sh
@@ -2,10 +2,12 @@
 
 # Create necessary directories if they don't exist
 mkdir -p ~/.config
+mkdir -p ~/.config/gtk-3.0
 mkdir -p ~/.config/i3status-rust
 mkdir -p ~/.config/sway
 
 # Does not require sudo
+cp $PWD/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp $PWD/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
 cp $PWD/sway/config ~/.config/sway/config
 cp $PWD/.zshrc ~/.zshrc
